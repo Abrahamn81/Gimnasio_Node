a4812cae2608181526e7b1d34e5ac73865737abc
Abraham <abrahamnm@hotmail.com>
2023-04-27 20:12:03 +0200

    se acabó

--- db/populateDB.js
+++ db/populateDB.js
@@ -8,27 +8,32 @@ async function main() {
   try {
     connection = await getConnection();
 
-    await connection.query(`
-      USE trabajoNode;
-    `);
+    // await connection.query(`
+    //   USE trabajoNode;
+    // `);
+    console.log("---");
 
-    console.log("Creando tablas...");
+    console.log("Creando contenido...");
 
     const passwordHash = await bcrypt.hash("123456789", 8);
 
     await connection.query(
       `
     INSERT INTO users (email, password, name, admin)
-    VALUES ('manolete@gmail.com', ?, 'Manolete', true),
-    ('patricia@gmail.com', ?, 'Patricia', false);
-
-    `,
-      [passwordHash, passwordHash]
+    VALUES ('manolete@gmail.com', '${passwordHash}', 'Manolete', true),
+    ('patricia@gmail.com', '${passwordHash}', 'Patricia', false);
+    `
     );
 
     await connection.query(`
         INSERT INTO exercises (name, category, idUser, img, description)
-        VALUES ('Abductores en polea', 'Pierna', 1, 'abductorespoela.jpg', 'Ejercicio de empuje en el que trabajamos los abductores, con un rango amplio para involucrar todas las fibras con una tensión constante gracias a una polea.') ;
+        VALUES ('Abductores en polea', 'Pierna', 1, 'abductorespoela.jpg', 'Ejercicio de empuje en el que trabajamos los abductores, con un rango amplio para involucrar todas las fibras con una tensión constante gracias a una polea.'),
+        ('Sentadillas búlgaras', 'Pierna', 1, 'bulgaras.jpg', 'Ejercicio de piernas en el que trabajamos unilateralmente ambas piernas, cargando un peso en los hombros y bajando, con una pierna apoyada en el banco, hasta romper la vertical.'),
+        ('Press plano con mancuernas', 'Pecho', 1, 'pressmancuernas.jpg', 'Ejercicio básico de empuje en el que utilizamos el pectoral para mover las mancuernas a la vez, descendiendo hasta la paralela del pecho y haciendo énfasis en la fase concéntrica'),
+        ('Curl de bíceps en banco', 'Brazos', 1, 'curlbicepsbanco.jpg', 'Ejercicio de brazos en el que trabajamos el rango total de recorrido del Bíceps, con gran énfasis en conseguir la mayor tensión en el tramo final del apriete'),
+        ('Curl de bíceps en polea baja', 'Brazos', 1, 'curlpoleabaja.jpg', 'Ejercicio de bíceps en el que trabajamos la zona superior gracias a la tensión constante que nos proporciona la polea, manteniendo la espalda recta y sacando bien el pecho'),
+        ('Dominadas', 'Espalda', 1, 'dominadas.jpg', 'Ejercicio de espalda por antonomasia en el que utilizamos nuestro propio peso de lastre, intentando sacar el pecho y tirar de las dorsales para una mejor ejecución'),
+        ('Remo gironda', 'Espalda', 1, 'remogironda.jpg', 'Ejercicio de tracción en el que con una polea, tiramos con ayuda de nuestras dorsales de una carga hacia el pecho, con especial enfoque en la negativa');
     
     `);
 
@@ -43,7 +48,7 @@ async function main() {
         );
     
     `); */
-    console.log("Tablas creadas!");
+    console.log("Contenido creado!");
   } catch (error) {
     console.error(error);
   } finally {
